72270cde4664f7871a71a40f2385b765a21f0ad1
Kai Hatje <kai.hatje@app-akademie.com>
2024-02-25 00:36:14 +0100

    Added wishlist page.

--- lib/features/wishlist/presentation/wishlist_page.dart
+++ lib/features/wishlist/presentation/wishlist_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:grocery_task/common/presentation/wishlist_controller.dart';
+import 'package:grocery_task/features/wishlist/presentation/wishlist_item.dart';
+import 'package:provider/provider.dart';
 
 class WishlistPage extends StatelessWidget {
   const WishlistPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final wishlistController = context.watch<WishlistController>();
+    final products = wishlistController.products;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView.separated(
+        itemCount: products.length,
+        itemBuilder: (context, index) => WishlistItem(product: products[index]),
+        separatorBuilder: (context, index) => const SizedBox(height: 12.0),
+      ),
+    );
   }
 }
